676e8ff6e62d34b887d1c1cbba15faa7e1bb904b
carlos soriano <mauricio092297@gmail.com>
2024-02-15 20:15:30 -0600

    primer compromiso

---
+++ lib/models/cast_crew.dart
@@ -0,0 +1,20 @@
+class CastCrew{
+  int id;
+  String name;
+  String subName;
+  String imagePath;
+  String personType;
+
+  CastCrew(
+{
+    required this.id,
+    required this.name,
+    required this.subName,
+    required this.imagePath,
+    required this.personType
+}
+  );
+
+
+
+}
